9ba4cdc06299cc9374a078350fff09147edb70b4
heshanmadv <157682942+heshanmadv@users.noreply.github.com>
2025-05-09 14:16:22 +0530

    Initialize task submission service with Maven setup and model

---
+++ TaskManeger/task-submission-service/src/test/java/com/submissionServiceM3/task_submission_service/TaskSubmissionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.submissionServiceM3.task_submission_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSubmissionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
